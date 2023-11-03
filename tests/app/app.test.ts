462f4fc752c0df67bd074ccb7edb61dbb9f1c0ff
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2023-11-03 15:00:21 +0100

    test: remove e2e test

--- tests/app/app.test.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('demo', async ({ page }) => {
-  await page.goto('http://localhost:5173/index.html');
-  expect(await page.title()).toContain('React ROI Demo');
-});
