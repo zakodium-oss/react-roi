67ff93662cdc0ca81f15d7f22e0c0d275d8fcfaa
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2023-11-10 14:29:51 +0100

    docs: add stories who update a label
    close: https://github.com/zakodium-oss/react-roi/issues/54

---
+++ stories/example/label.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta } from '@storybook/react';
+
+import {
+  RoiContainer,
+  RoiList,
+  RoiProvider,
+  useRoiActions,
+  useRoiState,
+} from '../../src';
+import { initialRois } from '../actions/utils';
+import { Image } from '../utils/Image';
+import { Layout } from '../utils/Layout';
+
+export default {
+  title: 'Example',
+} as Meta;
+
+export function UpdateLabel() {
+  // eslint-disable-next-line react/no-unstable-nested-components
+  function UpdateLabelButton() {
+    const { selectedRoi } = useRoiState();
+    const { updateRoi } = useRoiActions();
+
+    function onClick() {
+      updateRoi(selectedRoi, { label: 'Hello, World!' });
+    }
+
+    return (
+      <button type="button" onClick={onClick}>
+        Update label
+      </button>
+    );
+  }
+
+  return (
+    <RoiProvider initialRois={initialRois}>
+      <Layout>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
+          <UpdateLabelButton />
+          <RoiContainer target={<Image />}>
+            <RoiList />
+          </RoiContainer>
+        </div>
+      </Layout>
+    </RoiProvider>
+  );
+}
