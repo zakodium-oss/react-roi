7b4d67d17f1f29e31ca21c2ccc924688be0bd437
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2023-11-13 15:43:17 +0100

    refactor: remove unused function

--- src/utilities/assert.ts
+++ src/utilities/assert.ts
@@ -10,14 +10,6 @@ export function assertUnreachable(x: never, message?: string): never {
   );
 }
 
-export function assertNotNull<T>(
-  value: T | null,
-): asserts value is Exclude<T, null> {
-  if (value === null) {
-    throw new Error('unexpected null value');
-  }
-}
-
 export function assert(value: unknown, message?: string): asserts value {
   if (!value) {
     throw new Error(`unreachable${message ? `: ${message}` : ''}`);
