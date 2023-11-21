b10896209dfd9689e3f8981bb7cb9fcbe6eefdac
Daniel Kostro <kostro.d@gmail.com>
2023-11-21 10:20:36 +0100

    chore: fix playwright tests

--- tests/components/test.test.tsx
+++ tests/components/test.test.tsx
@@ -1,19 +1,21 @@
-import { test, expect } from '@playwright/experimental-ct-react';
+import { expect, test } from '@playwright/experimental-ct-react';
 
 import { TestComponent } from '../TestComponent';
 
-test('load initial rois', async ({ mount }) => {
-  const component = await mount(<TestComponent />);
-  const box1 = component.locator('_react=Box[id="box-1"]');
-  const box2 = component.locator('_react=Box[id="box-2"]');
+test('load initial rois', async ({ mount, page }) => {
+  await mount(<TestComponent />);
+  const label1 = page.getByTestId('label-box-1');
+  const label2 = page.getByTestId('label-box-2');
+  const box1 = page.getByTestId('box-1');
+  const box2 = page.getByTestId('box-2');
   // box 1
-  await expect(box1).toHaveText('box1');
+  await expect(label1).toHaveText('box1');
   await expect(box1).toHaveCSS('top', '0px');
   await expect(box1).toHaveCSS('left', '0px');
   await expect(box1).toHaveCSS('height', '250px');
   await expect(box1).toHaveCSS('width', '250px');
   // box 2
-  await expect(box2).toHaveText('box2');
+  await expect(label2).toHaveText('box2');
   await expect(box2).toHaveCSS('top', '350px');
   await expect(box2).toHaveCSS('left', '350px');
   await expect(box2).toHaveCSS('height', '150px');
