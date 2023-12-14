3f4de2c7490add923e0ed5b5d8662309d01aca8d
Daniel Kostro <kostro.d@gmail.com>
2023-12-14 09:21:04 +0100

    chore: remove code comments

--- src/hooks/usePanZoom.ts
+++ src/hooks/usePanZoom.ts
@@ -8,13 +8,6 @@ export function usePanZoom() {
 }
 
 export function usePanZoomTransform() {
-  // const {
-  //   panZoom: { scale: s1, translation: t1 },
-  //   initialPanZoom: { scale: s2, translation: t2 },
-  // } = usePanZoom();
-  // const scale = s1 * s2;
-
   const panZoom = computeTotalPanZoom(usePanZoom());
-  // return `matrix(${scale}, 0, 0, ${scale}, ${t1[0]}, ${t1[1]})`;
   return `matrix(${panZoom.scale}, 0, 0, ${panZoom.scale}, ${panZoom.translation[0]}, ${panZoom.translation[1]})`;
 }
