c1b989bfbae96dc371aeeb626eed3b01b24bd62f
Daniel Kostro <kostro.d@gmail.com>
2024-03-01 08:39:28 +0100

    fix: adapt resize cursors to rotation angle

--- src/utilities/corners.ts
+++ src/utilities/corners.ts
@@ -12,64 +12,80 @@ export interface CornerData {
   cy: number;
 }
 
+// Expects angle input ranging from -PI to +PI
+function getCursorIndex(angle: number, idx: number) {
+  return (Math.floor(((17 * Math.PI) / 8 + angle) / (Math.PI / 4)) + idx) % 8;
+}
+
+const cursors: Array<Exclude<CSSProperties['cursor'], undefined>> = [
+  'nwse-resize',
+  'ns-resize',
+  'nesw-resize',
+  'ew-resize',
+  'nwse-resize',
+  'ns-resize',
+  'nesw-resize',
+  'ew-resize',
+];
+function getCursor(angle: number, idx: number) {
+  return cursors[getCursorIndex(angle, idx)];
+}
+// (angle + Math.PI/8) / (Math.PI / 4) => 0-7
+
 export function getAllCorners(roi: CommittedBox): CornerData[] {
   const { x, y, width, height } = roi;
-  return [
+  const cursors: Array<Omit<CornerData, 'cursor'>> = [
     {
       xPosition: 'left',
       yPosition: 'top',
-      cursor: 'nwse-resize',
       cx: x,
       cy: y,
     },
     {
-      xPosition: 'left',
-      yPosition: 'bottom',
-      cursor: 'nesw-resize',
-      cx: x,
-      cy: y + height,
+      xPosition: 'middle',
+      yPosition: 'top',
+      cx: x + width / 2,
+      cy: y,
     },
     {
-      xPosition: 'right',
-      yPosition: 'bottom',
-      cursor: 'nwse-resize',
-      cx: x + width,
-      cy: y + height,
-    },
-    {
-      cursor: 'nesw-resize',
       xPosition: 'right',
       yPosition: 'top',
       cx: x + width,
       cy: y,
     },
     {
-      xPosition: 'middle',
-      yPosition: 'top',
-      cursor: 'ns-resize',
-      cx: x + width / 2,
-      cy: y,
-    },
-    {
-      xPosition: 'middle',
-      yPosition: 'bottom',
-      cursor: 'ns-resize',
-      cx: x + width / 2,
-      cy: y + height,
-    },
-    {
-      xPosition: 'left',
+      xPosition: 'right',
       yPosition: 'middle',
-      cursor: 'ew-resize',
-      cx: x,
+      cx: x + width,
       cy: y + height / 2,
     },
     {
       xPosition: 'right',
-      yPosition: 'middle',
-      cursor: 'ew-resize',
+      yPosition: 'bottom',
       cx: x + width,
+      cy: y + height,
+    },
+    {
+      xPosition: 'middle',
+      yPosition: 'bottom',
+      cx: x + width / 2,
+      cy: y + height,
+    },
+    {
+      xPosition: 'left',
+      yPosition: 'bottom',
+      cx: x,
+      cy: y + height,
+    },
+    {
+      xPosition: 'left',
+      yPosition: 'middle',
+      cx: x,
       cy: y + height / 2,
     },
   ];
+  return cursors.map((item, idx) => ({
+    ...item,
+    cursor: getCursor(roi.angle, idx),
+  }));
 }
